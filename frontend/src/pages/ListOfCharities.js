5e2f91ec9749f18e64250a86253aa64adcd93380
Hanguyen0911 <s3924594@rmit.edu.vn>
2023-12-29 00:38:08 +0700

    init setState and fetching data in ListOfCharities

--- frontend/src/pages/ListOfCharities.js
+++ frontend/src/pages/ListOfCharities.js
@@ -1,10 +1,45 @@
+import { response } from "express";
 import NavBar from "../components/NavBar"
+import { useState , useEffect } from "react"
+import { useNavigate } from "react-router-dom";
 
 export default function ListOfCharities() {
+
+    // Set states and effects
+
+    const navigate = useNavigate();
+    const [campaign, setCampaign] = useState([]);
+    const [showCampaigns, setShowCampaigns] = useState(true);
+    const [original, setOriginal] = useState([]);
+
+    // Fetch campaign data from campaign backend database
+
+    const getData = async () => {
+        const url = "http://localhost:2222/api/campaign";
+        const data = await response.json();
+        setCampaign();
+        setOriginal();
+        try {
+            const response = await fetch(url);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    // Web UI
+
     return (
         <div>
-            <NavBar/>
-            <h1>List of Charities</h1>
+            <div>
+                {/* Search Function */}
+                {/* Filter Function */}
+                <NavBar/>
+            </div>
+            <div>
+                {/* Display ongoing Charities */}
+                <h1>List of Charities</h1>
+            </div>
+            
         </div>
     )
 }
