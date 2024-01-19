cfe01035893cb7c3324c0d3bd21731654525dfdc
THuy Chi <chi.hathuy15@gmail.com>
2024-01-19 15:00:20 +0700

    css for create and edit campaign pages

--- frontend/src/pages/CreateCampaign.js
+++ frontend/src/pages/CreateCampaign.js
@@ -64,68 +64,72 @@ export default function CreateCampaign() {
     return (
         <div>
             <NavBar/>
-            <h1>Create Campaign</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Name of Campaign: </label> <br/>
-                <input type="text" name="name" onChange={e => setName(e.target.value)} 
-                value={name} placeholder="Enter Campaign's name" required/><br/>
-                
-                <label>Location:</label><br/>
-                <input type="text" name="location" onChange={e => setLocation(e.target.value)} 
-                value={location} placeholder="Enter The Location" required/><br/>
-                
-                <label>Description:</label><br/>
-                <textarea 
-                    value={description} 
-                    onChange={e => setDescription(e.target.value)} 
-                    placeholder="Tell more about the campaign..." 
-                    rows={5}
-                    cols={50}
-                    required
-                /><br/>
+            <div class="wrapper">
+                <h1>Create Campaign</h1>
+                <div class="form">
+                    <form onSubmit={handleSubmit}>
+                        <label>Name of Campaign: </label> <br/>
+                        <input type="text" name="name" onChange={e => setName(e.target.value)} 
+                        value={name} placeholder="Enter Campaign's name" required/><br/>
+                        
+                        <label>Location:</label><br/>
+                        <input type="text" name="location" onChange={e => setLocation(e.target.value)} 
+                        value={location} placeholder="Enter The Location" required/><br/>
+                        
+                        <label>Description:</label><br/>
+                        <textarea 
+                            value={description} 
+                            onChange={e => setDescription(e.target.value)} 
+                            placeholder="Tell more about the campaign..." 
+                            rows={5}
+                            cols={50}
+                            required
+                        /><br/>
 
-                <label>Type of Donate (You can select more than 1):</label><br/>   
-                <input 
-                    type="checkbox" 
-                    id="typeOfDonation1" value="Money" 
-                    checked={includeMoney} 
-                    onChange={e => {
-                        setIncludeMoney(e.target.checked);
-                    }} />
-                <label htmlFor="typeOfDonation1">Money </label>
-                <input 
-                    type="checkbox" 
-                    id="typeOfDonation2" value="Item"
-                    checked={includeItem}
-                    onChange={e => {
-                        setIncludeItem(e.target.checked);
-                    }}/>
-                <label htmlFor="typeOfDonation2">Item</label><br></br>
-                
-                {includeMoney && (
-                    <>
-                    <label>Goal (How much?):</label><br/>
-                    <input type="number" name="goal" onChange={e => setMoneyGoal(e.target.value)} 
-                    value={moneyGoal} min={0} placeholder="Enter The Goal" required/><br/>
-                    </>   
-                )}
-                {includeItem && (
-                    <>
-                        <label>Goal (What type of items?):</label><br/>
-                        <input type="text" name="goal" value={physicalGoal} 
-                        onChange={e => setPhysicalGoal(e.target.value)} placeholder="Enter the type of item" required/> <br/>
-                    </>    
-                )}
+                        <label>Type of Donate (You can select more than 1):</label><br/>   
+                        <input 
+                            type="checkbox" 
+                            id="typeOfDonation1" value="Money" 
+                            checked={includeMoney} 
+                            onChange={e => {
+                                setIncludeMoney(e.target.checked);
+                            }} />
+                        <label htmlFor="typeOfDonation1">Money </label>
+                        <input 
+                            type="checkbox" 
+                            id="typeOfDonation2" value="Item"
+                            checked={includeItem}
+                            onChange={e => {
+                                setIncludeItem(e.target.checked);
+                            }}/>
+                        <label htmlFor="typeOfDonation2">Item</label><br></br>
+                        
+                        {includeMoney && (
+                            <>
+                            <label>Goal (How much?):</label><br/>
+                            <input type="number" name="goal" onChange={e => setMoneyGoal(e.target.value)} 
+                            value={moneyGoal} min={0} placeholder="Enter The Goal" required/><br/>
+                            </>   
+                        )}
+                        {includeItem && (
+                            <>
+                                <label>Goal (What type of items?):</label><br/>
+                                <input type="text" name="goal" value={physicalGoal} 
+                                onChange={e => setPhysicalGoal(e.target.value)} placeholder="Enter the type of item" required/> <br/>
+                            </>    
+                        )}
 
-                <label>Start date:</label><br/>
-                <input type="date" name="startDate" onChange={e => setStartDate(e.target.value)} 
-                value={startDate} required/><br/>
+                        <label>Start date:</label><br/>
+                        <input type="date" name="startDate" onChange={e => setStartDate(e.target.value)} 
+                        value={startDate} required/><br/>
 
-                <label>End date:</label><br/>
-                <input type="date" name="password" onChange={e => setEndDate(e.target.value)} 
-                value={endDate} required/><br/>
-                <input type="submit" />
-            </form>
+                        <label>End date:</label><br/>
+                        <input type="date" name="password" onChange={e => setEndDate(e.target.value)} 
+                        value={endDate} required/><br/>
+                        <input type="submit" />
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
